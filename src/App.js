7835864a4a6782d2a2ea112fc97369ccde360c8a
63csmith <63csmith@gmail.com>
2022-11-02 12:25:29 -0600

    auth test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 
-function App({ signOut, user }) {
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className="App">
-      {user.attributes.name}
-      <button onClick={signOut}>Sign Out</button>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default withAuthenticator(App);
